3441f878cbc29f325b88b1262e71952e9eab0b5f
namirabal <mir17004@byui.edu>
2018-02-22 21:12:00 -0300

    franklin 7

---
+++ assigments/Franklin-7/scripts/currentdate.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script> 
+    var mydate = new Date(); 
+    var year = mydate.getYear( ); 
+    if (year < 1000) 
+    year += 1900 
+    var day = mydate.getDay(); 
+    var month = mydate.getMonth(); 
+    var daym = mydate.getDate(); 
+    if (daym<10) 
+    daym="0"+daym 
+    var dayarray=new Array("Sunday,","Monday,","Tuesday,","Wednesday,","Thursday,","Friday,","Saturday,")
+    var montharray=new Array("January","February","March","April","May","June","July","August","September","October","November","December")
+    document.write("<font color='black' face='Georgia' style='font-size:10pt'>"+dayarray[day]+" "+daym+"  "+montharray[month]+" "+year+" ") 
+</script>
+</body>
+</html>
