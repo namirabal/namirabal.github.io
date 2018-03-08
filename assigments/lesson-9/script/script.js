2e688bb559029e9805771d526a015ff9580ac6ef
namirabal <mir17004@byui.edu>
2018-03-08 20:59:33 -0300

    Json

--- assigments/lesson-9/script/script.js
+++ assigments/lesson-9/script/script.js
@@ -1,10 +1,16 @@
 var weatherObject= new XMLHttpRequest();
 
-weatherObject.open('GET','http://api.wunderground.com/api/Your_Key/conditions/q/CA/San_Francisco.json',true);
+weatherObject.open('GET','http://api.wunderground.com/api/4e2a1fd4930c6a3a/conditions/q/ID/Rexburg.json',true);
 
 weatherObject.send();
 
 weatherObject.onload = function(){
     var weatherInfo =JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
+    
+    document.getElementById('place').innerHTML = weatherInfo.current_observation.display_location.full;
+    
+    document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temp_f;
+    
+     document.getElementById('weather_icon').src = weatherInfo.current_observation.icon_url;    
 }//en of onload
