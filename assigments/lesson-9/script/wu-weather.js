fbb95658aa0485c7bda990a72ef56bac200619c3
namirabal <mir17004@byui.edu>
2018-03-09 15:54:44 -0300

    ya no mas

--- assigments/lesson-9/script/wu-weather.js
+++
@@ -1,9 +0,0 @@
-var requestURL = 'http://api.wunderground.com/api/4e2a1fd4930c6a3a/forecast/q/MN/Franklin.json';
-var request = new XMLHttpRequest();
-request.open('GET', requestURL, true);
-request.send();
-
-request.onload=function(){
-    var franklinWheather = JSON.parse(request.responseText);
-    
-   }
